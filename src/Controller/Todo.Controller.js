f31844ed0e0eb4cdac39a67d558f56885b79991b
vedansh tiwari <vt2855028@gmail.com>
2025-07-12 13:19:59 +0530

    Initial commit

--- src/Controller/Todo.Controller.js
+++ src/Controller/Todo.Controller.js
@@ -2,8 +2,19 @@ import Todo from "../model/Todo.Model.js";
 
 export const createTodo = async (req, res) => {
   try {
-    const taskCreate =  await Todo.create(req.body);
-    console.log("Todo created:", taskCreate);
+    // const taskCreate =  await Todo.create(req.body);
+    // console.log("Todo created:", taskCreate);
+
+    const { taskName, priority, status } = req.body;
+
+    const newTodo = new Todo({
+      taskName,
+      priority,
+      status,
+    });
+
+    const taskCreate = await newTodo.save();
+
     res.status(201).json({
       message: "Todo created successfully",
       data: taskCreate,
@@ -14,4 +25,60 @@ export const createTodo = async (req, res) => {
   }
 };
 
+export const getAllTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+
+
+
+    res.status(200).json({
+      message: "Todos fetched successfully",
+      data: todos,
+    });
+  } catch (err) {
+    console.error("Error fetching todos:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getTodoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findById(id);
+
+    console.log("Todo fetched by ID:", todo);
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res.status(200).json({
+      message: "Todo fetched successfully",
+      data: todo,
+    });
+  } catch (error) {
+    console.error("Error fetching todo by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteTodoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+
+    if (!deletedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res.status(200).json({
+      message: "Todo deleted successfully",
+      data: deletedTodo,
+    });
+    
+  } catch (error) {
+    console.error("Error fetching todo by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
--- src/routes/Todo.routes.js
+++ src/routes/Todo.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { createTodo } from "../Controller/Todo.Controller.js";
+import { createTodo, deleteTodoById, getAllTodos, getTodoById } from "../Controller/Todo.Controller.js";
 
 
 
 const router = express.Router();
 
 router.post("/create",createTodo);
+router.get("/getAll",getAllTodos);
+router.get("/todo/:id",getTodoById);
+router.delete("/todo/:id",deleteTodoById);
 
 
 export default router;
